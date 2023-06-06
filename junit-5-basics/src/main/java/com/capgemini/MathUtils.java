3c0e8b3b4b1ba3d6b35329d1f5ccef5f65ca52d1
chandra-98a <chandrakalaa2022@gmail.com>
2023-06-06 15:33:08 +0530

    "using assertThrows"

--- junit-5-basics/src/main/java/com/capgemini/MathUtils.java
+++ junit-5-basics/src/main/java/com/capgemini/MathUtils.java
@@ -5,6 +5,15 @@ public class MathUtils {
 	public int add(int a, int b) {
 		return a+b;
 	}
+	public int sub(int a, int b) {
+		return a-b;
+	}
+	public int mul(int a,int b) {
+		return a*b;
+	}
+	public int  div(int a, int b) {
+		return a/b;
+	}
 	
 	public double computeCircleArea(double radius) {
 		return 3.14*radius*radius;
--- junit-5-basics/src/test/java/com/capgemini/MathUtilsTest.java
+++ junit-5-basics/src/test/java/com/capgemini/MathUtilsTest.java
@@ -9,13 +9,18 @@ import org.junit.jupiter.api.Test;
 class MathUtilsTest {
 
 	@Test
-	void test() {
+	void testAdd() {
 		MathUtils mathUtils=new MathUtils();
 		int expected=2;
 		int actual = mathUtils.add(1, 1);
 		assertEquals(expected,actual,"The add method should add two numbers");	
-
 }
+	@Test
+	void testDivide() {
+		MathUtils mathUtils=new MathUtils();
+		assertThrows(NullPointerException.class,()-> mathUtils.div(1, 0),"divide by zero should throw");
+		
+	}
 	
 	@Test
 	void testComputeCircleArea() {
